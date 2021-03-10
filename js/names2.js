5b22a7379d84e16319626780b1ef9bfc0d712081
Craig Richards <craig@greywidget.co.uk>
2021-03-10 17:50:02 +0000

    Finished Task 3

---
+++ js/names2.js
@@ -0,0 +1,15 @@
+function buildName(firstName, lastName) {
+    return `${firstName} ${lastName}`;
+}
+
+
+function getName() {
+    var firstName = document.getElementById('firstName').value;
+    var lastName = document.getElementById('surname').value;
+    if (firstName == "" || lastName == "") {
+        return;
+    }
+
+    fullName = buildName(firstName, lastName);
+    document.getElementById('fullName').innerHTML = fullName;
+}
